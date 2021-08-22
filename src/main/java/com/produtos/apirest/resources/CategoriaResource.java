f0379e8f07c2124a9c278d8177cd3dc655fe2c86
Leonardo Araujo <araujoleonardo.ti@gmail.com>
2021-08-22 15:11:10 -0400

    Delete e Put concluidos
    Implementação dos métodos Delete e Put

--- src/main/java/com/produtos/apirest/resources/CategoriaResource.java
+++ src/main/java/com/produtos/apirest/resources/CategoriaResource.java
@@ -3,9 +3,11 @@ package com.produtos.apirest.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +34,16 @@ public class CategoriaResource {
 	
 	@PostMapping("/categoria")
 	public Categoria salvaCategoria(@RequestBody Categoria categoria) {
+		return categoriaRepository.save(categoria);
+	}
+	
+	@DeleteMapping("/categoria")
+	public void deletaCategoria(@RequestBody Categoria categoria) {
+		categoriaRepository.delete(categoria);
+	}
+	
+	@PutMapping("/categoria")
+	public Categoria atualizaCategoria(@RequestBody Categoria categoria) {
 		return categoriaRepository.save(categoria);
 	}
 
